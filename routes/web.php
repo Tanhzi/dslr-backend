9f73bfaa85b23b5fc599d450fd95a662d0a65721
TanhZi <mainkun2004@gmail.com>
2025-11-27 00:54:21 +0700

    update-28-11

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MediaController;
-use App\Console\Commands\AutoCleanupMedia;
 use Illuminate\Support\Facades\Artisan;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 // Thêm route này để hiển thị trang download
 Route::get('/download', [MediaController::class, 'showDownloadPage']);
-Route::get('/run-cleanup', function () {
-    // 🔒 BẢO MẬT: Chỉ cho phép từ IP của Render hoặc có token
-    if (!app()->isLocal() && !hash_equals('secret123xyz!@#', request()->query('token'))) {
+
+Route::get('/cleanup-trigger', function () {
+    // 🔒 Bảo mật bằng token (bắt buộc)
+    if (!hash_equals('k8d9#Lm2$vPq!xR5', request()->query('token'))) {
         abort(403);
     }
 
     Artisan::call('media:cleanup');
-    return response()->json([
-        'message' => 'Cleanup completed at ' . now(),
-        'output' => Artisan::output()
-    ]);
-})->name('cleanup');
+    return 'OK - Cleanup done at ' . now();
+})->name('cleanup.trigger');
